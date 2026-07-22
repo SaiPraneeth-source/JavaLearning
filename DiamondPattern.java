dfbf9a2e507b6317ce36fa8e29c07f3093a7207e
Praneeth <saipraneeth.adimulam@gmail.com>
2026-07-22 21:46:38 +0530

    Modified Class Names

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,4 +1,4 @@
-class Pattern2{
+class DiamondPattern{
 
 public static void main(String args[]){
 
--- NumberPattern.java
+++ NumberPattern.java
@@ -1,4 +1,4 @@
-class Pattern1{
+class NumberPattern{
 
 
 public static void main(String args[]){
