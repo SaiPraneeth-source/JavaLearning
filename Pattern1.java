1f673fee1a51fbee49f0f09b20c3812e497dc901
Praneeth <saipraneeth.adimulam@gmail.com>
2026-07-22 21:34:16 +0530

    Patterns

---
+++ Pattern1.java
@@ -0,0 +1,35 @@
+class Pattern1{
+
+
+public static void main(String args[]){
+
+int n=4;
+int i;
+int j;
+
+
+for (i=1;i<=4;i++){
+
+for (j=1;j<=i;j++){
+
+System.out.print(j);
+}
+
+
+
+for ( j = 1; j <= 2 * (n - i); j++){
+
+System.out.print(" ");
+}
+
+
+for(j=i;j>=1;j--){
+System.out.print(j);
+}
+System.out.println();
+}
+}
+
+}
+
+
---
+++ Pattern2.java
@@ -0,0 +1,51 @@
+class Pattern2{
+
+public static void main(String args[]){
+
+
+int n=5;
+int i;
+int j;
+
+for (i=1;i<=n;i++){
+
+for(j=1;j<=n-i;j++){
+
+System.out.print(" ");
+
+}
+
+for (j=1;j<=i;j++){
+
+System.out.print("* ");
+
+
+
+}
+System.out.println();
+
+}
+
+for(i=n;i>=1;i--){
+
+for(j=1;j<=n-i;j++){
+System.out.print("* ");
+
+
+}
+
+
+
+System.out.println();
+
+
+}
+
+
+
+
+
+
+
+}
+}
