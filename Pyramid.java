797c230870ff4b90783bbda1de7bf420269d0f81
Praneeth <saipraneeth.adimulam@gmail.com>
2026-07-26 09:15:27 +0530

    New Code

--- Pyramid.java
+++ Pyramid.java
@@ -5,27 +5,30 @@ public static void main(String args[]){
 
 int i;
 int j;
-int s; 
+int rows = 6;
+
  
 
-for( i = 1 ; i <= 6 ; i++ ) {
+for( i = 2; i <= rows; i+=2 ) {
 	
-	for( j = 6; j > i; j--) {
-		if( i == 1 || i == 2) {
-			System.out.print(" ");
+	for( j = 1; j <= 2; j++ ) {
+		
+			for(int s = rows; s >= i; s--) {
+		System.out.print(" ");
 		}
 		
-		else if ( i == 3 || i == 4) {
-			
+		for(int k = 1; k <= i; k++) {
 		System.out.print("* ");
-	}
+		}
+		
 	
-	else if (i == 5 || i == 6) {
-		System.out.print(" ");
+			
+			System.out.println();
 	}
 
 	
+		
 }
 }
 }
-}
+
